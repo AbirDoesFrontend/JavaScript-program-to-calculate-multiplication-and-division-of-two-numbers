0b2a5b864f29632629efa942fcce4f370aacfc89
Abir <ak.cr852456@gmail.com>
2022-01-29 02:51:48 +0600

    Program

---
+++ main.js
@@ -0,0 +1,20 @@
+// Multiplication Function
+
+function multiplyBy(){
+
+ numOne = document.getElementById('firstNumber').value;
+ numTwo = document.getElementById('secondNumber').value;
+
+ document.getElementById('result').innerHTML = numOne * numTwo;
+
+}
+
+
+// Division Function
+
+function divideBy(){
+ numOne = document.getElementById('firstNumber').value;
+ numTwo = document.getElementById('secondNumber').value;
+
+ document.getElementById('result').innerHTML = numOne / numTwo;
+}
